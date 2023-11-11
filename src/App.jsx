1e1bd758ae97dbb90d794c9bdba0bbc618640049
Siddhant <siddhantrsen73938@gmail.com>
2023-11-12 01:03:54 +0530

    Routing and Account page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Account from './components/Account'
 import ProtectedRoute from './components/ProtectedRoute'
 // import UserContext from './context/UserContext'
 import { AuthContextProvider } from './context/AuthContext'
+import Home from './SubRoutes/Home'
+import TimeTable from './SubRoutes/TimeTable'
+import Marksheet from './SubRoutes/Marksheet'
+import Calendar from './SubRoutes/Calendar'
+import Profile from './SubRoutes/Profile'
+import Settings from './SubRoutes/Settings'
 
 function App() {
 
   return (
     <div>
       <AuthContextProvider>
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
@@ -18,11 +25,19 @@ function App() {
             path='/account'
             element={
               <ProtectedRoute>
-                <Account />
+                <Account>
+                  <Route path='/account' element={<Home />} />
+                  <Route path='/account/timetable' element={<TimeTable />} />
+                  <Route path='/account/marksheet' element={<Marksheet />} />
+                  <Route path='/account/calendar' element={<Calendar />} />
+                  <Route path='/account/profile' element={<Profile />} />
+                  <Route path='/account/settings' element={<Settings />} />
+                </Account>
               </ProtectedRoute>
             } />
 
         </Routes>
+        </BrowserRouter>
       </AuthContextProvider>
     </div>
   )
---
+++ src/SubRoutes/TimeTable.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar"
+
+const TimeTable = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default TimeTable
