5d9969cf97648ef91d7cdee0c8ba2b7ac572e07d
Siddhant <siddhantrsen73938@gmail.com>
2023-11-11 19:23:35 +0530

    Adding protected route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Account from './components/Account'
+import ProtectedRoute from './components/ProtectedRoute'
 // import UserContext from './context/UserContext'
 import { AuthContextProvider } from './context/AuthContext'
 
@@ -13,7 +14,14 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
-          <Route path='/account' element={<Account />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            } />
+
         </Routes>
       </AuthContextProvider>
     </div>
